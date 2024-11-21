a1aea3916d71c5aaf24379b2604af600a8bc5c2c
entity12208 <116198380+entity12208@users.noreply.github.com>
2024-11-21 13:18:01 -0800

    Create linux.sh

---
+++ imager/linux.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/linux-dev/linux --linux.zip linux
+cd linux
+zip unzip linux
+sudo apt-get install --build with [rar, linux, packager]
+build linux-dev[latest]
+package linux-dev --run
